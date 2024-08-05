7333d5f66dda910a2778db7ef4a63405b1ca9903
whats2000 <whats2000mc@gmail.com>
2024-08-05 22:08:14 +0800

    Update vscode.ts

--- VSCodeExtension/code-brt/__mocks__/vscode.ts
+++ VSCodeExtension/code-brt/__mocks__/vscode.ts
@@ -1,1 +1,15 @@
-export default {};
+module.exports = {
+  window: {
+    showInformationMessage: jest.fn(),
+    showErrorMessage: jest.fn(),
+  },
+  workspace: {
+    workspaceFolders: [
+      {
+        uri: {
+          fsPath: 'tests/testsWorkspace',
+        },
+      },
+    ],
+  },
+};
