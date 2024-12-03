dc5643822d86f852973fa87942a646f2b99447a1
whats2000 <whats2000mc@gmail.com>
2024-12-03 09:58:05 +0800

    Add: Add the send to chat to editor menu

--- VSCodeExtension/code-brt/src/api/registerCodeActions.ts
+++ VSCodeExtension/code-brt/src/api/registerCodeActions.ts
@@ -5,8 +5,8 @@ import { triggerEvent } from './registerView';
 export const registerCodeActions = () => {
   vscode.languages.registerCodeActionsProvider('*', {
     provideCodeActions(
-      document: vscode.TextDocument,
-      range: vscode.Range,
+      _document: vscode.TextDocument,
+      _range: vscode.Range,
       _context: vscode.CodeActionContext,
       _token: vscode.CancellationToken,
     ): vscode.ProviderResult<(vscode.CodeAction | vscode.Command)[]> {
@@ -14,41 +14,37 @@ export const registerCodeActions = () => {
         'Refers to CodeBRT Chat',
         vscode.CodeActionKind.QuickFix,
       );
-      const relativePath = vscode.workspace.asRelativePath(document.fileName);
       sendCodeToChatAction.command = {
-        title: 'Refers to CodeBRT Chat',
         command: 'code-brt.sendCodeToChat',
-        arguments: [
-          document.getText(range),
-          range.start.line,
-          range.end.line,
-          document.languageId,
-          relativePath,
-        ],
+        title: 'Send code to chat',
       };
+      sendCodeToChatAction.isPreferred = true;
       return [sendCodeToChatAction];
     },
   });
 
-  vscode.commands.registerCommand(
-    'code-brt.sendCodeToChat',
-    (
-      codeText: string,
-      startLine: number,
-      endLine: number,
-      codeLanguage: string,
-      relativePath: string,
-    ) => {
-      if (codeText) {
-        triggerEvent('sendCodeToChat', {
-          id: Date.now().toString(),
-          codeText,
-          startLine,
-          endLine,
-          codeLanguage,
-          relativePath,
-        });
-      }
-    },
-  );
+  vscode.commands.registerCommand('code-brt.sendCodeToChat', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    const document = editor.document;
+    const codeText = editor.document.getText(editor.selection);
+    // Because the line number is 0-based, we need to add 1 to the line number
+    const startLine = editor.selection.start.line + 1;
+    const endLine = editor.selection.end.line + 1;
+    const codeLanguage = document.languageId;
+    const relativePath = vscode.workspace.asRelativePath(document.fileName);
+
+    if (codeText) {
+      triggerEvent('sendCodeToChat', {
+        id: Date.now().toString(),
+        codeText,
+        startLine,
+        endLine,
+        codeLanguage,
+        relativePath,
+      });
+    }
+  });
 };
