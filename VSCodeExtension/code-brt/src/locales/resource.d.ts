1208b6eb8e1dcd20fe97108ecb470c5b2717a411
whats2000 <60466660+whats2000@users.noreply.github.com>
2024-12-13 01:30:03 +0800

    Add: Add i18n for ModelAdvanceSettingFormItem

--- VSCodeExtension/code-brt/src/locales/resource.d.ts
+++ VSCodeExtension/code-brt/src/locales/resource.d.ts
@@ -17,6 +17,9 @@ interface Resources {
     "notSet": "Not Set",
     "voice": "Voice",
     "github": "GitHub",
+    "load": "Load",
+    "save": "Save",
+    "setDefault": "Set Default",
     "toolBar": {
       "generalSettings": "General Settings",
       "voiceSettings": "Voice Settings",
@@ -82,9 +85,64 @@ interface Resources {
       "selectVoice": "Select a voice",
       "gptSoVitsVoiceConfig": "GPT-SoVits Voice Configuration",
       "gptSoVitsVoiceLabel": "(GPT-SoVits)",
-      "gptSoVitsLearnMore": "Find out more about set up GPT-SoVits client host",
+      "gptSoVitsLearnMore": "Find out more about how to set up GPT-SoVits model locally",
       "selectReferenceVoice": "Select a reference voice",
       "gptSoVitsAdvanceSettings": "GPT-SoVits Advance Settings"
+    },
+    "GptSoVitsSettingsBar": {
+      "title": "GPT-SoVits Settings",
+      "clientHostLabel": "Client Host",
+      "clientHostExample": "(e.g. http://127.0.0.1:9880/)",
+      "clientHostNote": "Note: If you are using the old API V1, the default host is http://127.0.0.1:9880/, for V2, the default host is http://127.0.0.1:9880/tts/",
+      "selectedReferenceVoiceLabel": "Selected Reference Voice",
+      "selectReferenceVoicePlaceholder": "Select a reference voice",
+      "referenceVoicesLabel": "Reference Voices",
+      "addVoiceButton": "Add Voice",
+      "note": "Note: Chinese also support English content above GPT-SoVITS-beta0706 version."
+    },
+    "gptSoVitsSettingsBarSortableItem": {
+      "newVoice": "New Voice",
+      "nameLabel": "Name",
+      "referWavPathLabel": "Refer WAV Path",
+      "referTextLabel": "Refer Text",
+      "promptLanguageLabel": "Prompt Language",
+      "englishLabel": "English",
+      "chineseLabel": "Chinese",
+      "japaneseLabel": "Japanese"
+    },
+    "codeCompletionSettingsBar": {
+      "title": "Code Completion Settings",
+      "manualConfig": "Manually Trigger Configuration",
+      "manualTriggerLabel": "Manual Trigger Code Completion",
+      "showMoreInfoTooltip": "Click to show more information",
+      "enableManualTrigger": "Enable Manual Trigger Code Completion",
+      "manualModelServiceLabel": "Model Service for Manual Code Completion",
+      "manualModelLabel": "Model for Manual Code Completion",
+      "editKeybinding": "Edit Keybinding",
+      "manualDescription": "This will use more context, resources and token to provide higher quality completions.",
+      "autoConfig": "Auto Trigger Configuration",
+      "autoTriggerLabel": "Auto Trigger Code Completion",
+      "enableAutoTrigger": "Enable Auto Trigger Code Completion",
+      "autoModelServiceLabel": "Model Service for Auto Code Completion",
+      "ollamaTooltip": "Currently only support ollama",
+      "autoModelLabel": "Model for Auto Code Completion",
+      "autoModelTooltip": "We currently support the model which are in <supportModels /> fill hole model template. Please help us to add more model in GitHub if you interested.",
+      "autoDescription": "This will use when you typing pause for a moment. Which mean will faster suit for simple code snippet."
+    },
+    "modelAdvanceSettingBar": {
+      "descriptions": {
+        "systemPrompt": "The system prompt for the model. This can be used to introduce context to the model, such as a description of the conversation so far. Or, you can make the model more professional or casual by changing the prompt.",
+        "maxTokens": "The maximum number of tokens that can be generated in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length. What is token? Tokens are words, character sets, or combinations of words and punctuation that are used by large language models (LLMs) to decompose text into.",
+        "temperature": "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or `top_p` or `top_k` but not more than one.",
+        "topP": "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or `temperature` or `top_k` but not more than one.",
+        "topK": "An alternative to sampling with temperature, called top-k sampling, where the model considers the results of the top k most likely tokens. So 2 means that only the top 2 most likely tokens are considered. We generally recommend altering this or `temperature` or `top_p` but not more than one.",
+        "presencePenalty": "Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.",
+        "frequencyPenalty": "Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.",
+        "stop": "A list of tokens where the model should stop generating further tokens. Separate each token with a new line for multiple tokens. If you want to stop the model from generating more tokens, you can add a stop token here."
+      },
+      "notes": {
+        "topK": "Some models may not support top-k sampling."
+      }
     }
   },
   "userGuildTours": {
