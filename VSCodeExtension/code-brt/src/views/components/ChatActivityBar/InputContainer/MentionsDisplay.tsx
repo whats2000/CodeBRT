337428803942fbf0ec6387f79f3973cd977873f9
whats2000 <whats2000mc@gmail.com>
2025-01-01 00:59:33 +0800

    Feat: Update the display of the mention

--- VSCodeExtension/code-brt/src/views/components/ChatActivityBar/InputContainer/MentionsDisplay.tsx
+++ VSCodeExtension/code-brt/src/views/components/ChatActivityBar/InputContainer/MentionsDisplay.tsx
@@ -8,6 +8,7 @@ import {
   CodeOutlined,
   ExclamationCircleOutlined,
 } from '@ant-design/icons';
+import { useTranslation } from 'react-i18next';
 
 const SelectedCodeContainer = styled.div`
   display: flex;
@@ -34,39 +35,63 @@ const getMentionIcon = (mention: string) => {
 };
 
 const MentionCard = (props: { mention: string; onRemove: () => void }) => {
+  const { t } = useTranslation('common');
   const { mention, onRemove } = props;
   const icon = getMentionIcon(mention);
+  let cleanMention = mention
+    .replace('@problem:', '')
+    .replace('#folder:', '')
+    .replace('#file:', '');
 
-  return (
-    <StyledCard
-      size='small'
-      title={
-        <Tooltip title={mention}>
-          <Space>
-            {icon}
-            <Typography.Text>
-              {mention
-                .replace('problem:', '')
-                .replace('terminal:', '')
-                .replace('#folder:', '')
-                .replace('#file:', '')
-                .split(/[\\/]/)
-                .pop()}
-            </Typography.Text>
-          </Space>
-        </Tooltip>
-      }
-      extra={<CloseOutlined onClick={onRemove} />}
-      style={{ width: '100%' }}
-    />
-  );
+  if (mention.startsWith('@')) {
+    return (
+      <StyledCard
+        size='small'
+        title={
+          <Tooltip title={mention}>
+            <Space>
+              {icon}
+              <Typography.Text>
+                {t(`inputMessageArea.${cleanMention}Problem`)}
+              </Typography.Text>
+            </Space>
+          </Tooltip>
+        }
+        extra={<CloseOutlined onClick={onRemove} />}
+        style={{ width: '100%' }}
+      />
+    );
+  }
+  if (mention.startsWith('#')) {
+    let shorterMention = cleanMention;
+    if (mention.includes('/') || mention.includes('\\')) {
+      shorterMention = cleanMention.split(/[\\/]/).pop() || '';
+    }
+
+    return (
+      <StyledCard
+        size='small'
+        title={
+          <Tooltip title={mention}>
+            <Space>
+              {icon}
+              <Typography.Text>{shorterMention}</Typography.Text>
+              <Typography.Text type='secondary'>{cleanMention}</Typography.Text>
+            </Space>
+          </Tooltip>
+        }
+        extra={<CloseOutlined onClick={onRemove} />}
+        style={{ width: '100%' }}
+      />
+    );
+  }
 };
 
 export const MentionsDisplay: React.FC<{
   inputMessage: string;
   setInputMessage: (value: string) => void;
 }> = ({ inputMessage, setInputMessage }) => {
-  const mentionRegex = /[@#][\w-]+:[\w-\\/.]+/g;
+  const mentionRegex = /[@#][\w-]+:[\w-\\/.]+\s/g;
   const [visibleMentions, setVisibleMentions] = useState<string[]>([]);
 
   useEffect(() => {
